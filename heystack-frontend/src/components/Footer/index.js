a95659d18eb12eea249b54b89170dd109e0f31be
Dragoner <70686717+chuhuangXY@users.noreply.github.com>
2023-10-31 12:13:36 +0800

    Added social component

--- heystack-frontend/src/components/Footer/index.js
+++ heystack-frontend/src/components/Footer/index.js
@@ -1,4 +1,5 @@
 import NavBar from "../NavBar";
+import Social from "../Social/index.js";
 import './style.css';
 
 const Footer = () => {
@@ -9,6 +10,7 @@ const Footer = () => {
                 Copyright @ 2023
                 Heystack Pte Ltd
             </div>
+            <Social />
         </div>
     );
 }
---
+++ heystack-frontend/src/components/Social/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+const Social = () => {
+
+    return (
+        <div>
+            <a href="https://example.com" class="icon instagram"></a>
+            <a href="https://example.com" class="icon facebook"></a>
+            <a href="https://example.com" class="icon twitter"></a>
+        </div>
+    )
+}
+
+export default Social;
